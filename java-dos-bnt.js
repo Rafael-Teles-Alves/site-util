1445a23f78dffb908e79ccb7baa0cbc66847bcfc
Rafael Teles dos santos Alves <rafael_tsalves@hotmail.com>
2023-07-07 00:45:14 -0300

    11

---
+++ java-dos-bnt.js
@@ -0,0 +1,29 @@
+// javascript para remover coisas//
+function removerCaracteres() {
+    var inputText = document.getElementById("input-text").value;
+
+        // Utilizando expressão regular para substituir os caracteres desejados
+    var outputText = inputText.replace(/[* ,.,\/]/g,'');
+
+    document.getElementById("output-text").textContent = outputText;
+    }
+    //javasript do bnt copiar //
+
+window.addEventListener('DOMContentLoaded', function() {
+    var copyButton = document.querySelector('.copy-button');
+    var textoCopiado = document.getElementById('output-text');
+
+    copyButton.addEventListener('click', function() {
+        var textoSelecionado = window.getSelection();
+        var range = document.createRange();
+
+        range.selectNode(textoCopiado);
+        textoSelecionado.removeAllRanges();
+        textoSelecionado.addRange(range);
+
+        document.execCommand('copy');
+
+        document.getElementById("alert-text").innerHTML = "Pronto Copiado"
+        
+    });
+    });
